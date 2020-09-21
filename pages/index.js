7f2df9b8bf90d9fc70373a070d0fdd8d8c262d38
Yahya Chahine <chahineyahya@pm.me>
2020-09-21 19:43:48 +0100

    🎉 Tailwind and Mapbox setup

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,24 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
 
 export default function Home() {
+  const [viewport, setViewport] = useState({
+    latitude: 33.5731104,
+    longitude: -7.5898434,
+    zoom: 8,
+    width: '100vw',
+    height: '100vh',
+  });
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    <div>
+      <ReactMapGL
+        {...viewport}
+        mapStyle="mapbox://styles/ceeyahya/ckfcsse1e14se19ozntzg4m56"
+        mapboxApiAccessToken={process.env.NEXT_PUBLIC_API_KEY}
+        onViewportChange={(viewport) => setViewport(viewport)}
+      >
+        marker
+      </ReactMapGL>
     </div>
-  )
+  );
 }
